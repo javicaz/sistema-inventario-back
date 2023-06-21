d4e651133cb931c265fd263c4d3aee4915654d08
Oscar Guerra <oogl29@gmail.com>
2023-06-20 22:48:08 -0500

    Create the first configuration api

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log("Server Listening on PORT:", PORT);
+});
+
+app.get("/status", (request, response) => {
+    const status = {
+        "Status": "Running"
+    };
+
+    response.send(status);
+});
